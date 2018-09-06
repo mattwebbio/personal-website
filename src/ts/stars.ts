3c93be209cbd2b7cf8447a214dad12c3fecd1824
Matt Webb <matt531320.efreak@gmail.com>
2018-09-06 13:48:00 -0700

    the stars are alive!!!

--- src/ts/stars.ts
+++ src/ts/stars.ts
@@ -13,8 +13,25 @@ class stars {
         return this.size;
     }
 
-    getStar(i: number): star {
-        return this.starray[i];
+    moveStar(i: number): void {
+        if (i > this.getSize() || i < 0) return;
+        else this.starray[i].move();
+    }
+
+
+    getStarX(i: number, width: number): number {
+        if (i > this.getSize() || i < 0) return -1000;
+        else return Math.round(this.starray[i].getX() * width);
+    }
+
+    getStarY(i: number, height: number): number {
+        if (i > this.getSize() || i < 0) return -1000;
+        else return Math.round(this.starray[i].getY() * height);
+    }
+
+    getStarZ(i: number): number {
+        if (i > this.getSize() || i < 0) return -1000;
+        else return Math.round(this.starray[i].getZ());
     }
 }
 
@@ -22,11 +39,12 @@ class star {
     private x: number;
     private y: number;
     private z: number;
+    private XY_INCREMENT: number = 0.5;
+    private Z_INCREMENT: number = 1.5;
+    private MARGIN: number = 10;
 
     constructor() {
-        this.x = Math.round(Math.random() * 10000)/100;
-        this.y = Math.round(Math.random() * 10000)/100;
-        this.z = Math.round(Math.random() * 10000)/100;
+        this.reset();
     }
     getLoc(): Array<number> {
         return [this.getX(), this.getY(), this.getZ()];
@@ -38,35 +56,60 @@ class star {
         return this.y/100;
     }
     getZ(): number {
-        return this.z/100;
+        if (this.z < 0) return 0;
+        else return this.z;
+    }
+    reset(): void {
+        this.x = Math.round(Math.random() * 10000)/100;
+        this.y = Math.round(Math.random() * 10000)/100;
+        this.z = Math.round(Math.random() * -100);
     }
     move(): void {
+        this.x += ((this.x - 50)/50) + this.XY_INCREMENT;
+        this.y += ((this.y - 50)/50) + this.XY_INCREMENT;
+        this.z += this.Z_INCREMENT;
 
+        if (this.y > (100 + this.MARGIN) || this.y < (0 - this.MARGIN) || 
+        this.x > (100 + this.MARGIN) || this.x < (0 - this.MARGIN)){
+            this.reset();
+        }
     }
 }
 
-function draw(canvas: CanvasRenderingContext2D){
-    for (let i = 0; i < start.getSize(); i++){
-        let loc: Array<number> = start.getStar(i).getLoc();
-        canvas.beginPath();
-        canvas.strokeStyle="white";
-        canvas.rect(Math.round(loc[0] * starfield.width), 
-            Math.round(loc[1] * starfield.height), 2, 2);
-        canvas.stroke();
+function render(container: HTMLCanvasElement, starfield: stars): void {
+    let canvas = container.getContext("2d");
+    canvas.clearRect(0, 0, container.width, container.height);
+
+    for (let i = 0; i < starfield.getSize(); i++){
+        let x = starfield.getStarX(i, container.width),
+            y = starfield.getStarY(i, container.width),
+            z = Math.round(starfield.getStarZ(i)/100 * 5);
+
+        if (x < container.width && y < container.height){
+            canvas.beginPath();
+            canvas.fillStyle="white";
+            canvas.fillRect(x, y, z, z);
+            canvas.stroke();
+        }
+
+        starfield.moveStar(i);
     }
 }
 
-let start = new stars(100);
-let starfield = <HTMLCanvasElement> document.getElementById("starfield");
+function nextFrame() {
+    render(container, starfield);
+    return setTimeout(function(){window.requestAnimationFrame(nextFrame)}, 1000/30);
+}
 
+let starfield = new stars(500);
+let container = <HTMLCanvasElement> document.getElementById("starfield");
 
-// Match Hero size
 window.onload = function() {
     let heroHeight = document.getElementById("hero").clientHeight;
-    starfield.style.height = heroHeight + "px";
-    starfield.width  = starfield.offsetWidth;
-    starfield.height = starfield.offsetHeight;
-    let c = starfield.getContext("2d");
+    // Match Hero size
+    container.style.height = heroHeight + "px";
+    container.width  = container.offsetWidth;
+    container.height = container.offsetHeight;
 
-    draw(c);
+    window.requestAnimationFrame(nextFrame);
 }
